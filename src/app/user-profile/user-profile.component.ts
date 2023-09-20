f331f70f3625a53de14606a985e691343a1cd9ce
kaser1997 <kaser.slaiby@gmail.com>
2023-09-20 10:13:23 +0300

    modified

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,5 +1,6 @@
-import { DataService } from './../../service/data.service';
 import { Component, OnInit } from '@angular/core';
+import { DataService } from './../../service/data.service';
+
 
 @Component({
   selector: 'app-user-profile',
