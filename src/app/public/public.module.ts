266d6ac7f7eed7b36be9a70686d1d9072ccdd977
kaser1997 <kaser.slaiby@gmail.com>
2023-09-26 09:52:15 +0300

    add public component

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PublicRoutingModule } from './public-routing.module';
+import { PublicComponent } from './public/public.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PublicComponent
+  ],
   imports: [
     CommonModule,
     PublicRoutingModule
