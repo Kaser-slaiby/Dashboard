f37fda04f64304a5b7d7ddcb14e2c28484541aad
kaser1997 <kaser.slaiby@gmail.com>
2023-09-26 12:30:29 +0300

    add changeDetection & OnInit

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 }
