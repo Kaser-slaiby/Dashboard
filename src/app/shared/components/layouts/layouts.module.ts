1c45a768dec6120f81e6bd738f89120331d1e8ae
kaser <kaser.slaiby@gmail.com>
2023-09-28 16:30:27 +0300

    create c  layout

--- src/app/shared/components/layouts/layouts.module.ts
+++ src/app/shared/components/layouts/layouts.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
+import { UserLayoutComponent } from './user-layout/user-layout.component';
+import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
+import { BlankLayoutComponent } from './blank-layout/blank-layout.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AdminLayoutComponent,
+    UserLayoutComponent,
+    AuthLayoutComponent,
+    BlankLayoutComponent
+  ],
   imports: [
     CommonModule
   ]
