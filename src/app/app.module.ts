825c1203926df751c6aed1c34c103bd119884f28
kaser <kaser.slaiby@gmail.com>
2023-09-28 16:52:11 +0300

    import Shared in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgbAlertModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     BrowserAnimationsModule,
     NgbModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
