31fd979164d3618fa28f5b8be3484e3db99e6a5b
kaser <kaser.slaiby@gmail.com>
2023-09-28 16:50:59 +0300

    import Layout in shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { LayoutsModule } from './components/layouts/layouts.module';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    LayoutsModule
   ],
   exports: [
-    NavbarComponent
   ]
 })
 export class SharedModule { }
