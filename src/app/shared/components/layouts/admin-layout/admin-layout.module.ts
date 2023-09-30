15c6453d351565d082e5d1af3066bb08d19e2bc5
kaser <kaser.slaiby@gmail.com>
2023-09-30 11:05:27 +0300

    modified sidebar

--- src/app/shared/components/layouts/admin-layout/admin-layout.module.ts
+++ src/app/shared/components/layouts/admin-layout/admin-layout.module.ts
@@ -4,6 +4,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AdminLayoutRoutes } from './admin-layout-routing.module';
+import {MatButtonModule} from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 
 @NgModule({
@@ -14,6 +20,12 @@ import { AdminLayoutRoutes } from './admin-layout-routing.module';
     CommonModule,
     RouterModule.forChild(AdminLayoutRoutes),
     FormsModule,
+    MatButtonModule,
+    MatRippleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatTooltipModule,
     ReactiveFormsModule,
     ]
 })
--- src/app/shared/components/layouts/layouts.module.ts
+++ src/app/shared/components/layouts/layouts.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UserLayoutComponent } from './user-layout/user-layout.component';
-import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
-import { BlankLayoutComponent } from './blank-layout/blank-layout.component';
+// import { UserLayoutComponent } from './user-layout/user-layout.component';
+// import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
+// import { BlankLayoutComponent } from './blank-layout/blank-layout.component';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from './footer/footer.component';
+// import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    UserLayoutComponent,
-    AuthLayoutComponent,
-    BlankLayoutComponent,
-    FooterComponent,
+    // UserLayoutComponent,
+    // AuthLayoutComponent,
+    // BlankLayoutComponent,
+    // FooterComponent,
   ],
   imports: [
     CommonModule,
