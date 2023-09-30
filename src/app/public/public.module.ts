0072953ed4638dd8d8d1d45e9df5ffd93e32a099
kaser <kaser.slaiby@gmail.com>
2023-09-30 04:12:31 +0300

    create Component dashboard

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -4,16 +4,21 @@ import { CommonModule } from '@angular/common';
 import { PublicRoutingModule } from './public-routing.module';
 import { PublicComponent } from './public.component';
 import { SharedModule } from '../shared/shared.module';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
-    PublicComponent
+    PublicComponent,
+    DashboardComponent
   ],
   imports: [
     CommonModule,
     PublicRoutingModule,
     SharedModule
+  ],
+  exports: [
+    DashboardComponent
   ]
 })
 export class PublicModule { }
